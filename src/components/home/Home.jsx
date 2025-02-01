6a6ba0bfc56b77d3a03181bca4ab97ef40f4ba9c
Abdullahi Abdulkadir <graphgeo02@gmail.com>
2025-02-01 07:40:04 +0100

    testing mobile responsive

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 const Home = () => {
+
+  let backRedir = useNavigate();
+
+ 
   return (
-    <div><h1>Home</h1></div>
+
+    <div><h1>
+          <button onClick={()=>backRedir("/cart")}>Back</button>
+      Home</h1></div>
   )
 }
 
